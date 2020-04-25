17e4931c69f9a5e8608b2b53d93ef7321a27173c
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-04-25 19:55:18 +0100

    Initial commit

---
+++ ILS.Library.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ILS.Library.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
