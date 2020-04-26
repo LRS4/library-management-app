64b61ff71f6472f38fe62042b94654633b89b2c6
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-04-26 17:44:27 +0100

    Updated checkout table descriptions

--- ILS.Library.Database/Asset/Checkout.sql
+++ ILS.Library.Database/Asset/Checkout.sql
@@ -23,3 +23,40 @@
 
 
 )
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'This references the library asset that was checked out',
+    @level0type = N'SCHEMA',
+    @level0name = N'Asset',
+    @level1type = N'TABLE',
+    @level1name = N'Checkout',
+    @level2type = N'COLUMN',
+    @level2name = N'LibraryAssetId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'This references the library card to which the asset was checked out',
+    @level0type = N'SCHEMA',
+    @level0name = N'Asset',
+    @level1type = N'TABLE',
+    @level1name = N'Checkout',
+    @level2type = N'COLUMN',
+    @level2name = N'LibraryCardId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'When the asset has been checked out since.',
+    @level0type = N'SCHEMA',
+    @level0name = N'Asset',
+    @level1type = N'TABLE',
+    @level1name = N'Checkout',
+    @level2type = N'COLUMN',
+    @level2name = N'Since'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'When the asset is checked out until.',
+    @level0type = N'SCHEMA',
+    @level0name = N'Asset',
+    @level1type = N'TABLE',
+    @level1name = N'Checkout',
+    @level2type = N'COLUMN',
+    @level2name = N'Until'
