f7acfd44d54c38f130a547110d0912defa781fce
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-04-26 20:28:28 +0100

    Added library asset service

---
+++ ILS.Library.Web/Services/Interfaces/ILibraryAsset.cs
@@ -0,0 +1,24 @@
+﻿using ILS.Library.DataAccess.SecurityDb.Entities.Asset;
+using ILS.Library.DataAccess.SecurityDb.Entities.Branch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILS.Library.Web.Services.Interfaces
+{
+    public interface ILibraryAsset
+    {
+        IEnumerable<LibraryAsset> GetAll();
+        LibraryAsset GetById(int id);
+
+        void Add(LibraryAsset newAsset);
+        string GetAuthorOrDirector(int id);
+        string GetDeweyIndex(int id);
+        string GetType(int id);
+        string GetTitle(int id);
+        string GetISBN(int id);
+
+        BranchDetails GetCurrentLocation(int id);
+    }
+}
---
+++ ILS.Library.Web/Services/LibraryAssetService.cs
@@ -0,0 +1,78 @@
+﻿using ILS.Library.DataAccess.SecurityDb.Entities;
+using ILS.Library.DataAccess.SecurityDb.Entities.Asset;
+using ILS.Library.DataAccess.SecurityDb.Entities.Branch;
+using ILS.Library.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILS.Library.Web.Services
+{
+    public class LibraryAssetService : ILibraryAsset
+    {
+
+        #region Private Properties
+        private readonly ILSContext _context;
+        #endregion
+
+        #region Constructor
+        public LibraryAssetService(ILSContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region ILibraryAssetService
+        /// <summary>
+        /// Adds a new library asset
+        /// </summary>
+        /// <param name="newAsset">The new library asset to be added</param>
+        public void Add(LibraryAsset newAsset)
+        {
+            _context.Add(newAsset);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<LibraryAsset> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAuthorOrDirector(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LibraryAsset GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BranchDetails GetCurrentLocation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDeweyIndex(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetISBN(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetType(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
