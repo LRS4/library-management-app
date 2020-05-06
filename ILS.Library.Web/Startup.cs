a4285785c081aeadefdbe1675244653ca0069b38
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-05-06 14:47:39 +0100

    Configured home view and added razor re-render to startup services

--- ILS.Library.Web/Startup.cs
+++ ILS.Library.Web/Startup.cs
@@ -27,7 +27,7 @@ namespace ILS.Library.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddMvc();
             services.AddSingleton(Configuration);
 
