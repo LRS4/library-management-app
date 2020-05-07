4a630b54bdd93eb058ac12a4143ddaa713c272ed
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-05-07 12:56:01 +0100

    Added view Patron details controller

--- ILS.Library.Web/Controllers/PatronController.cs
+++ ILS.Library.Web/Controllers/PatronController.cs
@@ -1,4 +1,6 @@
-﻿using ILS.Library.Web.Models.Patron;
+﻿using ILS.Library.DataAccess.SecurityDb.Entities.Asset;
+using ILS.Library.DataAccess.SecurityDb.Entities.Users;
+using ILS.Library.Web.Models.Patron;
 using ILS.Library.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -34,13 +36,35 @@ namespace ILS.Library.Web.Controllers
                 FirstName = p.FirstName,
                 LastName = p.LastName,
                 LibraryCardId = p.LibraryCardId,
-                OverdueFees = p.LibraryCard.Fees,
+                OverdueFees = Math.Round(p.LibraryCard.Fees, 2),
                 HomeLibraryBranch = p.HomeLibraryBranch.Name
             }).ToList();
 
             var model = new IndexViewModel()
             {
                 Patrons = patronModel
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int patronId)
+        {
+            var patron = _patronService.Get(patronId);
+
+            var model = new PatronDetailModel()
+            {
+                FirstName = patron.FirstName,
+                LastName = patron.LastName,
+                Address = patron.Address,
+                HomeLibraryBranch = patron.HomeLibraryBranch.Name,
+                MemberSince = patron.LibraryCard.Created,
+                OverdueFees = Math.Round(patron.LibraryCard.Fees, 2),
+                LibraryCardId = patron.LibraryCard.LibraryCardId,
+                Telephone = patron.TelephoneNumber,
+                AssetsCheckedOut = _patronService.GetCheckouts(patronId).ToList() ?? new List<Checkout>(),
+                CheckoutHistory = _patronService.GetCheckoutHistory(patronId),
+                Holds = _patronService.GetHolds(patronId)
             };
 
             return View(model);
