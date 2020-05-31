e659f0a08d6805ce2e01a5b865927eb467fde061
LRS4 <lrspencer@hotmail.co.uk>
2020-05-31 18:43:29 +0100

    Added branch and type enums

---
+++ ILS.Library.Web/Enums/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILS.Library.Web.Enums
+{
+    public enum Branch
+    { 
+        [Display(Name = "Lake Shore Branch")]
+        LakeShore = 1,
+        [Display(Name = "Mountain View Branch")]
+        MountainView = 2,
+        [Display(Name = "Pleasant Hill Branch")]
+        PleasantHill = 3
+    }
+}
---
+++ ILS.Library.Web/Enums/Discriminator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILS.Library.Web.Enums
+{
+    public enum Discriminator
+    {
+        [Display(Name = "Book")]
+        Book = 1,
+        [Display(Name = "Video")]
+        Video = 2
+    }
+}
